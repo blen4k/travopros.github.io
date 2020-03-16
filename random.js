e43ba271a179320823921389c7e7cd8061f46eb8
blen4k <39500987+blen4k@users.noreply.github.com>
2020-03-16 20:37:09 +0300

    Add files via upload

---
+++ random.js
@@ -0,0 +1,257 @@
+function generate() {
+    var random;
+        random = (Math.floor(Math.random() * (8 - 1) + 1)); // Генератор числа
+		if (random == 1) {
+  document.getElementById('random').innerHTML=('Маг');
+}
+	if (random == 2) {
+  document.getElementById('random').innerHTML=('Воин');
+}
+	if (random == 3) {
+  document.getElementById('random').innerHTML=('Стрелок');
+}
+	if (random == 4) {
+  document.getElementById('random').innerHTML=('Призыватель');
+}
+if (random == 5) {
+document.getElementById('random').innerHTML=('Воин/Цепник');
+}
+if (random == 6) {
+document.getElementById('random').innerHTML=('Метатель удачи в страданиях &#128513');
+}
+if (random == 7) {
+document.getElementById('random').innerHTML=('Пацифист(атакуют только нипы и механизиы)');
+}
+}
+//разделение от бленча
+
+
+function Narisovat() {
+    var Nar;
+        Nar = (Math.floor(Math.random() * (73 - 1) + 1)); // Генератор числа
+		//боссы
+  if (Nar == 1) {
+  document.getElementById('Nar').innerHTML=('Глаз Ктулху');
+}
+if (Nar == 2) {
+  document.getElementById('Nar').innerHTML=('Скелетон');
+}
+if (Nar == 3) {
+  document.getElementById('Nar').innerHTML=('Пожиратель миров');
+}
+if (Nar == 4) {
+  document.getElementById('Nar').innerHTML=('Мозг ктулху');
+}
+if (Nar == 5) {
+  document.getElementById('Nar').innerHTML=('Король слизень');
+}
+if (Nar == 6) {
+  document.getElementById('Nar').innerHTML=('Королева Пчел');
+}
+if (Nar == 7) {
+  document.getElementById('Nar').innerHTML=('Стена плоти');
+}
+if (Nar == 8) {
+  document.getElementById('Nar').innerHTML=('Механики вместе');
+}
+if (Nar == 9) {
+  document.getElementById('Nar').innerHTML=('Близнецы');
+}
+if (Nar == 10) {
+  document.getElementById('Nar').innerHTML=('Уничтожитель');
+}
+if (Nar == 11) {
+  document.getElementById('Nar').innerHTML=('Скелетон прайм');
+}
+if (Nar == 12) {
+  document.getElementById('Nar').innerHTML=('Плантера');
+}
+if (Nar == 13) {
+  document.getElementById('Nar').innerHTML=('Голем');
+}
+if (Nar == 14) {
+  document.getElementById('Nar').innerHTML=('Культист лунатик');
+}
+if (Nar == 15) {
+  document.getElementById('Nar').innerHTML=('Башня солнца');
+}
+if (Nar == 16) {
+  document.getElementById('Nar').innerHTML=('Башня туманности');
+}
+if (Nar == 17) {
+  document.getElementById('Nar').innerHTML=('Башня вихря');
+}
+if (Nar == 18) {
+  document.getElementById('Nar').innerHTML=('Башня звёздной пыли');
+}
+if (Nar == 19) {
+  document.getElementById('Nar').innerHTML=('Мунлорд');
+}
+//мобы
+if (Nar == 20) {
+  document.getElementById('Nar').innerHTML=('Слизень');
+}
+if (Nar == 21) {
+  document.getElementById('Nar').innerHTML=('Зомби');
+}
+if (Nar == 22) {
+  document.getElementById('Nar').innerHTML=('Глаз');
+}
+if (Nar == 23) {
+  document.getElementById('Nar').innerHTML=('Кримера');
+}
+if (Nar == 24) {
+  document.getElementById('Nar').innerHTML=('Пожиратель');
+}
+if (Nar == 25) {
+  document.getElementById('Nar').innerHTML=('Мимик');
+}
+if (Nar == 26) {
+  document.getElementById('Nar').innerHTML=('Единорог');
+}
+if (Nar == 27) {
+  document.getElementById('Nar').innerHTML=('Пикси');
+}
+if (Nar == 28) {
+  document.getElementById('Nar').innerHTML=('Бес');
+}
+if (Nar == 29) {
+  document.getElementById('Nar').innerHTML=('Демон вуду');
+}
+if (Nar == 30) {
+  document.getElementById('Nar').innerHTML=('Нимфа');
+}
+if (Nar == 31) {
+  document.getElementById('Nar').innerHTML=('Саламандра');
+}
+if (Nar == 32) {
+  document.getElementById('Nar').innerHTML=('Скелет шахтёр');
+}
+if (Nar == 33) {
+  document.getElementById('Nar').innerHTML=('Скелет из данжа');
+}
+if (Nar == 34) {
+  document.getElementById('Nar').innerHTML=('Виверна');
+}
+if (Nar == 35) {
+  document.getElementById('Nar').innerHTML=('Гарпия');
+}
+if (Nar == 36) {
+  document.getElementById('Nar').innerHTML=('Иллюзорный дракон');
+}
+if (Nar == 37) {
+  document.getElementById('Nar').innerHTML=('Пираты');
+}
+if (Nar == 38) {
+  document.getElementById('Nar').innerHTML=('Всадник без головы');
+}
+if (Nar == 39) {
+  document.getElementById('Nar').innerHTML=('Тыква');
+}
+if (Nar == 40) {
+  document.getElementById('Nar').innerHTML=('Санта-мк1');
+}
+//нипы
+if (Nar == 41) {
+  document.getElementById('Nar').innerHTML=('Гид');
+}
+if (Nar == 42) {
+  document.getElementById('Nar').innerHTML=('Торговец');
+}
+if (Nar == 43) {
+  document.getElementById('Nar').innerHTML=('Оружейник');
+}
+if (Nar == 44) {
+  document.getElementById('Nar').innerHTML=('Медсестра');
+}
+if (Nar == 45) {
+  document.getElementById('Nar').innerHTML=('Дриада');
+}
+if (Nar == 46) {
+  document.getElementById('Nar').innerHTML=('Подрывник');
+}
+if (Nar == 47) {
+  document.getElementById('Nar').innerHTML=('Старик');
+}
+if (Nar == 48) {
+  document.getElementById('Nar').innerHTML=('Портной');
+}
+if (Nar == 49) {
+  document.getElementById('Nar').innerHTML=('Волшебник');
+}
+if (Nar == 50) {
+  document.getElementById('Nar').innerHTML=('Механик');
+}
+if (Nar == 51) {
+  document.getElementById('Nar').innerHTML=('Гоблин-инжинер');
+}
+if (Nar == 52) {
+  document.getElementById('Nar').innerHTML=('Санта');
+}
+if (Nar == 53) {
+  document.getElementById('Nar').innerHTML=('Маляр');
+}
+if (Nar == 54) {
+  document.getElementById('Nar').innerHTML=('Паромеханик');
+}
+if (Nar == 55) {
+  document.getElementById('Nar').innerHTML=('Пират');
+}
+if (Nar == 56) {
+  document.getElementById('Nar').innerHTML=('Тусовщица');
+}
+if (Nar == 57) {
+  document.getElementById('Nar').innerHTML=('Знахарь');
+}
+if (Nar == 58) {
+  document.getElementById('Nar').innerHTML=('Киборг');
+}
+if (Nar == 59) {
+  document.getElementById('Nar').innerHTML=('Трюфель');
+}
+if (Nar == 60) {
+  document.getElementById('Nar').innerHTML=('Продавец красителей');
+}
+if (Nar == 61) {
+  document.getElementById('Nar').innerHTML=('Стилист');
+}
+if (Nar == 62) {
+  document.getElementById('Nar').innerHTML=('Странствующий торговец');
+}
+if (Nar == 63) {
+  document.getElementById('Nar').innerHTML=('Рыбак');
+}
+if (Nar == 64) {
+  document.getElementById('Nar').innerHTML=('Сборщик налогов');
+}
+if (Nar == 65) {
+  document.getElementById('Nar').innerHTML=('Скелет-торгаш');
+}
+if (Nar == 66) {
+  document.getElementById('Nar').innerHTML=('Трактирщик');
+}
+//бонус
+if (Nar == 67) {
+  document.getElementById('Nar').innerHTML=('Следующие 4 на одном арте');
+}
+if (Nar == 68) {
+  document.getElementById('Nar').innerHTML=('Гифани(агента)');
+}
+if (Nar == 69) {
+  document.getElementById('Nar').innerHTML=('Я против босса');
+}
+if (Nar == 70) {
+  document.getElementById('Nar').innerHTML=('🌚 шип npc ');
+}
+if (Nar == 72) {
+  document.getElementById('Nar').innerHTML=('Пасхалка от бленчика −• •− •−−• •• −−−− ••   −••• •−•• •• −• −−−• •• −•− ••−   −−−• − −−−   ••• −−− •−− −•−−   • −•• •−•− −   •− •−− −−− −•− •− −•• −−−   ');
+}
+//пока конец будет дополнено
+//люблю коменты
+//обожаю
+//велл привет как дела?
+if (Nar == 73) {
+  document.getElementById('Nar').innerHTML=('Культист лунатик');
+}
+
+}
